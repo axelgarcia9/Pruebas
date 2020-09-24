10cce9d6526901e15a63c977e4278c0ab226f336
Axel Adair Garcia Rodriguez <za17012277@zapopan.tecmm.edu.mx>
2020-09-24 12:57:16 -0500

    Ahora es una suma

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 
-
-def print_hi(name):
-
-    print(f'Hola, {name}')
-
+def suma():
+    num1 = int(input("Dame un numero"))
+    num2 = int(input("Dame otro numero"))
+    print("La suma es: ", num1 + num2)
 
 
 if __name__ == '__main__':
-    print_hi('Axel')
+    suma()
+
+
 
